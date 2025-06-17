81a34c8ed192fc7c22537c85be83eff3cfcb28b7
Giannis Argyropoulos <51941746+Argy22@users.noreply.github.com>
2025-06-17 12:33:26 +0300

    Delete 2nd Semester directory

--- 2nd Semester/SQL/Λύσεις Ασκήσεων/TASK_2_5254/5254_SELECT_ALL.sql
+++
@@ -1,7 +0,0 @@
-SELECT cAFM, cAT, cLastName, cFirstName, cRegDate FROM CUSTOMER;
-SELECT vPlateNumber, vColor, vCC, vHorsePower, vRentTimes, VehCatName FROM VEHICLE;
-SELECT Name, VehicleCategoryPrice, VehicleCount FROM VEHICLECAT;
-SELECT eName, ePrice FROM EXTRAS;
-SELECT cAFM, cPhone FROM PHONE;
-SELECT cAFM, vPlateNumber, OutDate, InDate FROM RENTING;
-SELECT vPlateNumber, eName FROM VEHICLE_EXTRAS;
